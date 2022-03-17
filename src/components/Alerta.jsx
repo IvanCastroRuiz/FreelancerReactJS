d7f7ae330d20fdc425422f120ef6d76c010b5091
IvanCastroRuiz <icastror@hotmail.com>
2022-03-16 22:44:24 -0500

    first commit

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,11 +1,15 @@
 
 const Alerta = ({alerta}) => {
   
+    setTimeout(() => {
+      setAlerta({});
+    }, 3000)    
+
     return (
       <div className={ ` ${ alerta.error ? 'error' : 'correcto'}`}>
         {alerta.msg}
       </div>
     )
-  }
+}
   
 export default Alerta
